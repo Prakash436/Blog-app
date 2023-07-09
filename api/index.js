ee81bdba10329730911888dee431a37259ee44dd
Prakash436 <prakashkumar43674@gmail.com>
2023-07-09 13:00:37 +0530

    Forth

--- api/index.js
+++ api/index.js
@@ -3,20 +3,55 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
+
+const salt = bcrypt.genSaltSync(10);
+const secret = 'nfdsajfnadjnksdksmfkmsdiofmdskvmds';
 
 mongoose.connect(process.env.mongoose_link)
 
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
     try {
-        const userDoc = await User.create({username, password});
-        res.json(userDoc);
+        const userDoc = await User.create({username, password:bcrypt.hashSync(password,salt),
+        });
+        res.cookie('token', token).json('ok');
     } catch(e) {
         res.status(400).json(e);
     }
 });
+
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOK = bcrypt.compareSync(password,userDoc.password);
+    if(passOK) {
+        jwt.sign({username, id:userDoc._id}, secret, {}, (err,token) => {
+            if(err) throw err;
+            res.cookie('token',token).json('ok');
+        });
+    } else{
+        res.status(400).json('wrong credentrials')
+    }
+})
+
+app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err,info)=>{
+        if(err) throw err;
+        res.json(info);
+    });
+    res.json(req.cookies);
+})
+
+app.post('/logout', (req,res) => {
+    res.cookie('token','').json('ok');
+})
 
 app.listen(4000)
--- client/src/header.js
+++ client/src/header.js
@@ -1,12 +1,41 @@
+import { useEffect, useState } from "react";
 import {Link} from "react-router-dom"
 
-export default function header() {
+export default function Header() {
+    const [username,setUsername] = useState(null);
+    useEffect(()=> {
+        fetch('http://localhost:4000/profile', {
+            credentials:'include',
+        }).then(response => {
+            response.json().then(userInfo => {
+                setUsername(userInfo.username);
+            })
+        })
+    },[]);
+    function Logout() {
+        fetch('http://localhost:4000/logout', {
+            credentials: 'include',
+            method: 'POST',
+        });
+        setUsername(null);
+    }
+
     return (
         <header>
             <Link to="/" class="logo">MyBlog</Link>
             <nav>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
+                {username && (
+                    <>
+                        <Link to="/create">Create new post</Link>
+                        <a onClick={Logout}>Logout</a>
+                    </>
+                )}
+                {!username && (
+                    <>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                    </>
+                )}
             </nav>
       </header>
     );
