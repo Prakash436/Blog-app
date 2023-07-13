77c4fc8ead714a97b523dab150cf4281434c5bf6
Prakash436 <prakashkumar43674@gmail.com>
2023-07-13 15:47:32 +0530

    sixth

--- api/index.js
+++ api/index.js
@@ -6,10 +6,16 @@ const User = require('./models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({dest: 'upload/' });
+const fs = require('fs');
+const Post = require('./models/Post');
+
 require('dotenv').config();
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
+app.use('/upload', express.static(__dirname + '/upload'))
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'nfdsajfnadjnksdksmfkmsdiofmdskvmds';
@@ -57,8 +63,35 @@ app.post('/logout', (req,res) => {
     res.cookie('token','').json('ok');
 });
 
-app.post('/post',(req,res) => {
-    
+app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length-1];
+    const newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, async (err,info)=>{
+        if(err) throw err;
+        const {title,summary,content} = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover:newPath,
+            author:info.id,
+        })
+        res.json(postDoc);
+    });
+})
+
+app.get('/post', async (req,res) => {
+    res.json(
+        await Post.find()
+            .populate('author', ['username'])
+            .sort({credentials:-1})
+            .limit(20)
+    );
+
 })
 
 app.listen(4000)
--- client/src/post.js
+++ client/src/post.js
@@ -1,16 +1,18 @@
-export default function Post() {
+import {format} from "date-fns"
+
+export default function Post({title,summary,cover,createdAt,author}) {
     return (
         <div className="post">
         <div className="image">
-          <img src="https://uploads-ssl.webflow.com/5b105a0c66f2f636c7884a17/64063dbcad97bd421b437096_chatgpt-p-800.jpg"alt=""/>
+          <img src={'http://localhost:4000/'+cover}alt=""/>
         </div>
         <div className="texts">
-          <h2>Chat gpt is good</h2>
+          <h2>{title}</h2>
           <div className="info">
-            <a href="/" className="author">David</a>
-            <time>2023-01-06 16:45</time>
+            <a href="/" className="author">{author.username}</a>
+            <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
           </div>
-          <p className="summary">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <p className="summary">{summary}</p>
         </div>
       </div>
     );
