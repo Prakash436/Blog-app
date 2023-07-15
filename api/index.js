ec49529ce5e44fcc3d578a23b94918078a67d8d7
Prakash436 <prakashkumar43674@gmail.com>
2023-07-15 13:56:31 +0530

    seventh

--- api/index.js
+++ api/index.js
@@ -10,6 +10,7 @@ const multer = require('multer');
 const uploadMiddleware = multer({dest: 'upload/' });
 const fs = require('fs');
 const Post = require('./models/Post');
+const path = require('path');
 
 require('dotenv').config();
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
@@ -82,6 +83,43 @@ app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
         })
         res.json(postDoc);
     });
+})
+
+app.put('/post', uploadMiddleware.single('file'),async(req,res) => {
+    let newPath = null;
+    if(req.file) {
+        const {originalname,path} = req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length-1];
+        newPath = path+'.'+ext;
+        fs.renameSync(path, newPath);
+    }
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, async (err,info)=>{
+        if(err) throw err;
+        const {id,title,summary,content} = req.body;
+        const postDoc = await Post.findById(id);
+        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+        let tempPath = 'upload/' + postDoc.cover.replace('upload\\', '');
+        if(!isAuthor){
+            return res.status(400).json('you are not the author');
+        }
+        else{
+            fs.unlinkSync(tempPath);
+        }
+        // if (isAuthor) {
+        //     tempPath = tempPath.replace('/','');
+        //     // fs.unlinkSync(tempPath);
+        //     // console.log(`File deleted ${tempPath}`);
+        // }
+        await postDoc.updateOne({
+            title,
+            summary,
+            content,
+            cover:newPath ? newPath : postDoc.cover,
+        });
+    });
+
 })
 
 app.get('/post', async (req,res) => {
--- client/src/pages/Editpost.js
+++ client/src/pages/Editpost.js
@@ -1,36 +1,51 @@
-import { useState} from 'react';
-import { Navigate } from "react-router-dom";
-import Editor from '../Editor';
+import { useEffect, useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
+import Editor from "../Editor";
 
-export default function CreatePost(){
+export default function EditPost() {
+    const {id} = useParams();
     const [title,setTitle] = useState('');
     const [summary,setSummary] = useState('');
     const [content,setContent] = useState('');
     const [files,setFiles] = useState('');
     const [redirect,setRedirect] = useState(false);
-    async function createNewPost(ev){
+
+    useEffect(() => {
+        fetch('http://localhost:4000/post/'+id)
+        .then(response => {
+            response.json().then(postInfo => {
+                setTitle(postInfo.title);
+                setContent(postInfo.content);
+                setSummary(postInfo.summary);
+            })
+        })
+    })
+
+    async function updatePost(ev) {
         const data = new FormData();
         data.set('title',title);
         data.set('summary',summary);
         data.set('content',content);
-        data.set('file',files[0]);
+        data.set('id',id);
+        if(files?.[0]){
+            data.set('file',files?.[0]);
+        }
         ev.preventDefault();
         const response = await fetch('http://localhost:4000/post', {
-            method:'POST',
+            method:'PUT',
             body:data,
-            credentials: 'include',
-        })
-        if (response.ok) {
+            credentials:'include',
+        });
+        if(response.ok){
             setRedirect(true);
         }
     }
 
     if (redirect) {
-        return <Navigate to={'/'} />
+        return <Navigate to={'/post/'+id} />
     }
-
     return (
-        <form onSubmit={createNewPost}>
+        <form onSubmit={updatePost}>
             <input type="title" 
             placeholder={"Title"}
             value={title}
@@ -41,8 +56,8 @@ export default function CreatePost(){
             onChange={ev => setSummary(ev.target.value)} />
             <input type="file"
             onChange={ev => setFiles(ev.target.files)}/>
-            <Editor value={content} onChange={setContent} />
-            <button style={{marginTop:'1vh'}}>Create post</button>
+            <Editor onChange={setContent} value={content} />
+            <button style={{marginTop:'1vh'}}>Update post</button>
         </form>
     )
 }
